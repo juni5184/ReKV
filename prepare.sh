1bc1f47a610b3ca31b86c5572a60999afb20c385
juni5184 <juni5184@yonsei.ac.kr>
2025-09-19 05:31:28 +0000

    update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,11 +1,24 @@
-conda create -n rekv python=3.11 -y
+conda create -n rekv 
 conda init
+
+# restart
 conda activate rekv 
 # conda activate rekvbash prepare.sh
 
-pip install -U torch torchvision torchaudio
+apt-get update
+apt-get install -y git vim tmux ffmpeg wget curl rsync git-lfs
+
+pip install transformers==4.52.0 qwen_vl_utils
+pip install accelerate pillow matplotlib bitsandbytes
+pip install decord opencv-python tqdm datasets
+pip install flash-attn --no-build-isolation
+# --no-build-isolation = 내 현재 환경을 그대로 써서 빌드 (PyTorch/CUDA 확장 빌드 시 필수)
+
+
+# /workspace/ReKV/
 pip install -U "git+https://github.com/huggingface/transformers.git@66bc4def9505fa7c7fe4aa7a248c34a026bb552b"
 pip install -e .
 
-cd model/LongVA/longva/
+cd model/longva/
 pip install -e .
+
