d4498bf79b0122a3b999652f855008ade157729f
juni5184 <juni5184@naver.com>
2025-11-03 14:12:03 +0900

    fix: longva directory path modified

--- prepare.sh
+++ prepare.sh
@@ -5,5 +5,7 @@ pip install -U torch torchvision torchaudio
 pip install -U "git+https://github.com/huggingface/transformers.git@66bc4def9505fa7c7fe4aa7a248c34a026bb552b"
 pip install -e .
 
-cd model/LongVA/longva
+cd model/longva
 pip install -e .
+cd ../..
+
