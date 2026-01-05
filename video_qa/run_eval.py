3c7e264d09a556af952e06bc480e0b5d70826c39
juni5184 <juni5184@naver.com>
2026-01-05 17:35:15 +0900

    add debugger code

--- video_qa/run_eval.py
+++ video_qa/run_eval.py
@@ -16,39 +16,6 @@ def exec(cmd, sub=False, device=None):
         subprocess.run(cmd, env=my_env)
 
 
-def eval_mlvu(args):
-    num_chunks = args.num_chunks
-    save_dir = f"results/{args.model}/mlvu/{args.retrieve_size}-{args.sample_fps}"
-    solver = "rekv_offline_vqa"
-    if not args.only_eval:
-        # QA
-        processes = []
-        for idx in range(0, num_chunks):
-            cmd = ["python", f"video_qa/{solver}.py",
-                    "--model", args.model,
-                    "--sample_fps", str(args.sample_fps),
-                    "--n_local", str(args.n_local),
-                    "--retrieve_size", str(args.retrieve_size),
-                    "--save_dir", save_dir,
-                    "--anno_path", "data/mlvu/dev_debug_mc.json",
-                    "--debug", args.debug,
-                    "--num_chunks", str(num_chunks),
-                    "--chunk_idx", str(idx)]
-            p = multiprocessing.Process(target=exec, args=(cmd, True, f'{4*idx},{4*idx+1},{4*idx+2},,{4*idx+3}' if args.model=='llava_ov_72b' else str(idx)))  # llava_ov_72b needs 4x 80GB GPUs
-            processes.append(p)
-            p.start()
-        for p in processes:
-            p.join()
-        # merge results
-        exec(f"> {save_dir}/results.csv")
-        for idx in range(num_chunks):
-            if idx == 0:
-                exec(f"head -n 1 {save_dir}/{num_chunks}_{idx}.csv > {save_dir}/results.csv")
-            exec(f"tail -n +2 {save_dir}/{num_chunks}_{idx}.csv >> {save_dir}/results.csv")
-            exec(f"rm {save_dir}/{num_chunks}_{idx}.csv")
-    # eval
-    exec(f"python video_qa/eval/eval_multiple_choice.py --save_dir {save_dir}")
-
 def eval_qaego4d(args):
     num_chunks = args.num_chunks
     save_dir = f"results/{args.model}/qaego4d/{args.retrieve_size}-{args.sample_fps}"
@@ -63,8 +30,9 @@ def eval_qaego4d(args):
                     "--n_local", str(args.n_local),
                     "--retrieve_size", str(args.retrieve_size),
                     "--save_dir", save_dir,
+                    # "--anno_path", "data/qaego4d/test_mc.json",
                     "--anno_path", "data/qaego4d/test_mc_sample.json",
-                    "--debug", args.debug,
+                    "--debug", str(args.debug),
                     "--num_chunks", str(num_chunks),
                     "--chunk_idx", str(idx)]
             p = multiprocessing.Process(target=exec, args=(cmd, True, f'{4*idx},{4*idx+1},{4*idx+2},,{4*idx+3}' if args.model=='llava_ov_72b' else str(idx)))  # llava_ov_72b needs 4x 80GB GPUs
@@ -86,7 +54,7 @@ def eval_qaego4d(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type=str, default="llava_ov_7b", choices=['llava_ov_0.5b', 'llava_ov_7b'])
-    parser.add_argument("--dataset", type=str, default=None, choices=['mlvu', 'qaego4d'])
+    parser.add_argument("--dataset", type=str, default=None, choices=['qaego4d'])
     parser.add_argument("--num_chunks", type=int, default=1)
     parser.add_argument("--only_eval", action="store_true")
     parser.add_argument("--sample_fps", type=float, default=1)
@@ -94,10 +62,21 @@ if __name__ == "__main__":
     parser.add_argument("--retrieve_size", type=int, default=64)
     parser.add_argument("--debug", type=str, default='false')
     args = parser.parse_args()
+
+    if args.debug == 'true':
+        import debugpy
+        def connect_debugpy():
+            if not debugpy.is_client_connected():
+                debugpy.listen(("0.0.0.0", 2345))
+                print("Waiting for debugger to attach...")
+                debugpy.wait_for_client()
+            debugpy.configure(subProcess=True)
+        connect_debugpy()
+    
     func_dic = {
-        'mlvu': eval_mlvu,
         'qaego4d': eval_qaego4d,
     }
+    
     if args.dataset in func_dic:
         print(f'Execute {args.dataset} evaluation')
         func_dic[args.dataset](args)
