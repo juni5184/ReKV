692a0b2a10ad728dd8c25d0df47d751b076ba3e9
juni5184 <juni5184@naver.com>
2026-01-20 12:34:09 +0900

    concat qaego4d defs  & add mlvu def

--- video_qa/run_eval.py
+++ video_qa/run_eval.py
@@ -14,53 +14,14 @@ def exec(cmd, sub=False, device=None):
         my_env["CUDA_VISIBLE_DEVICES"] = device
         subprocess.run(cmd, env=my_env)
 
-def eval_qaego4d_vanilla(args):
-    num_chunks = args.num_chunks
-    save_dir = f"results/{args.model}_vanilla/qaego4d/{args.retrieve_size}-{args.sample_fps}"
-    solver = "vanilla_offline_vqa"
-    if args.sample:
-        anno_path = "data/qaego4d/test_mc_sample.json"
-    else:
-        anno_path = "data/qaego4d/test_mc.json"
-    if not args.only_eval:
-        processes = []
-        for idx in range(0, num_chunks):
-            cmd = [
-                "python", f"video_qa/{solver}.py",
-                "--model", args.model,
-                "--sample_fps", str(args.sample_fps),
-                "--retrieve_size", str(args.retrieve_size),
-                "--save_dir", save_dir,
-                "--anno_path", anno_path,
-                "--debug", str(args.debug),
-                "--num_chunks", str(num_chunks),
-                "--chunk_idx", str(idx),
-                "--solver", args.solver,
-            ]
-            p = multiprocessing.Process(
-                target=exec,
-                args=(
-                    cmd,
-                    True,
-                    f'{4*idx},{4*idx+1},{4*idx+2},{4*idx+3}' if args.model == 'llava_ov_72b' else str(idx)
-                )
-            )
-            processes.append(p)
-            p.start()
-        for p in processes:
-            p.join()
-        exec(f"> {save_dir}/results.csv")
-        for idx in range(num_chunks):
-            if idx == 0:
-                exec(f"head -n 1 {save_dir}/{num_chunks}_{idx}.csv > {save_dir}/results.csv")
-            exec(f"tail -n +2 {save_dir}/{num_chunks}_{idx}.csv >> {save_dir}/results.csv")
-            exec(f"rm {save_dir}/{num_chunks}_{idx}.csv")
-    exec(f"python video_qa/eval/eval_multiple_choice.py --save_dir {save_dir}")
 
-def eval_qaego4d_rekv(args):
+def eval_qaego4d(args):
     num_chunks = args.num_chunks
-    save_dir = f"results/{args.model}_rekv/qaego4d/{args.retrieve_size}-{args.sample_fps}"
-    solver = "rekv_offline_vqa"
+    save_dir = f"results/{args.model}/qaego4d/{args.solver}/{args.retrieve_size}-{args.sample_fps}"
+    if args.solver == 'rekv':
+        solver = "rekv_offline_vqa"
+    else:
+        solver = "vanilla_offline_vqa"
     if args.sample:
         anno_path = "data/qaego4d/test_mc_sample.json"
     else:
@@ -98,13 +59,59 @@ def eval_qaego4d_rekv(args):
             if idx == 0:
                 exec(f"head -n 1 {save_dir}/{num_chunks}_{idx}.csv > {save_dir}/results.csv")
             exec(f"tail -n +2 {save_dir}/{num_chunks}_{idx}.csv >> {save_dir}/results.csv")
-            exec(f"rm {save_dir}/{num_chunks}_{idx}.csv")
+    exec(f"python video_qa/eval/eval_multiple_choice.py --save_dir {save_dir}")
+
+
+def eval_mlvu(args):
+    num_chunks = args.num_chunks
+    save_dir = f"results/{args.model}/mlvu/{args.retrieve_size}-{args.sample_fps}"
+    if args.solver == 'rekv':
+        solver = "rekv_offline_vqa"
+    else:
+        solver = "vanilla_offline_vqa"
+    if args.sample:
+        anno_path = "data/mlvu/dev_debug_mc.json"
+    else:
+        anno_path = "data/mlvu/dev_mc.json"
+    if not args.only_eval:
+        processes = []
+        for idx in range(0, num_chunks):
+            cmd = [
+                "python", f"video_qa/{solver}.py",
+                "--model", args.model,
+                "--sample_fps", str(args.sample_fps),
+                "--n_local", str(args.n_local),
+                "--retrieve_size", str(args.retrieve_size),
+                "--save_dir", save_dir,
+                "--anno_path", anno_path,
+                "--debug", str(args.debug),
+                "--num_chunks", str(num_chunks),
+                "--chunk_idx", str(idx),
+                "--solver", args.solver,
+            ]
+            p = multiprocessing.Process(
+                target=exec,
+                args=(
+                    cmd,
+                    True,
+                    f'{4*idx},{4*idx+1},{4*idx+2},{4*idx+3}' if args.model == 'llava_ov_72b' else str(idx)
+                )
+            )
+            processes.append(p)
+            p.start()
+        for p in processes:
+            p.join()
+        exec(f"> {save_dir}/results.csv")
+        for idx in range(num_chunks):
+            if idx == 0:
+                exec(f"head -n 1 {save_dir}/{num_chunks}_{idx}.csv > {save_dir}/results.csv")
+            exec(f"tail -n +2 {save_dir}/{num_chunks}_{idx}.csv >> {save_dir}/results.csv")
     exec(f"python video_qa/eval/eval_multiple_choice.py --save_dir {save_dir}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type=str, default="llava_ov_7b", choices=['llava_ov_0.5b', 'llava_ov_7b'])
-    parser.add_argument("--dataset", type=str, default=None, choices=['qaego4d'])
+    parser.add_argument("--dataset", type=str, default=None, choices=['qaego4d', 'mlvu'])
     parser.add_argument("--num_chunks", type=int, default=1)
     parser.add_argument("--only_eval", action="store_true")
     parser.add_argument("--sample_fps", type=float, default=1)
@@ -126,13 +133,10 @@ if __name__ == "__main__":
         connect_debugpy()
     
     func_dic = {
-        'qaego4d_vanilla': eval_qaego4d_vanilla,
-        'qaego4d_rekv': eval_qaego4d_rekv,
+        'qaego4d': eval_qaego4d,
+        'mlvu': eval_mlvu,
     }
     
-    if args.dataset == 'qaego4d':
+    if args.dataset in func_dic:
         print(f'Execute {args.dataset} evaluation')
-        if args.solver == 'rekv':
-            func_dic['qaego4d_rekv'](args)
-        else:
-            func_dic['qaego4d_vanilla'](args)
+        func_dic[args.dataset](args)
